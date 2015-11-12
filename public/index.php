c72390e47f903259dbd6d1c651802a622f90b1b0
MarvinDurot <marvin.dut@gmail.com>
2015-11-12 23:58:33 +0100

    Reformat routes for REST API

--- public/index.php
+++ public/index.php
@@ -1,14 +1,32 @@
 <?php
 require '../vendor/autoload.php';
 use Core\Router\Router;
+use App\Tables\Stations;
+use App\Tables\Mesures;
 use App\App;
 
 $router = new Router($_GET['url']);
 
-$router->get('/', function() {
-    echo App::getInstance()->title;
+$router->get('/', function () {
+
 });
-$router->get('/stations', function() { echo 'Toutes les stations.'; });
-$router->get('/stations/:id', function($id) { echo "Afficher la station $id"; });
+
+$router->get('/stations', function () {
+    $controller = new \App\Controllers\ApiController();
+    $controller->stations();
+});
+
+$router->get('/stations/:id', function ($id) {
+    $controller =  new \App\Controllers\ApiController();
+    $controller->station($id);
+});
+
+$router->get('/releves/:station', function ($station) {
+
+    var_dump($station);
+
+    $controller =  new \App\Controllers\ApiController();
+    $controller->releves($station);
+});
 
 $router->run();
