f7c6aa36e77975e681afe496d6d19dbad20db46d
MarvinDuroy <marvin.dut@gmail.com>
2015-11-15 21:03:58 +0100

    Complete routes

--- public/index.php
+++ public/index.php
@@ -1,32 +1,60 @@
 <?php
 require '../vendor/autoload.php';
 use Core\Router\Router;
-use App\Tables\Stations;
-use App\Tables\Mesures;
-use App\App;
+use \App\Controllers\ApiController;
+use \App\Controllers\StationsController;
 
+define('ROOT', dirname(__DIR__));
+
+// Création du routeur
 $router = new Router($_GET['url']);
 
+/**
+ * Routage Interface Web 
+ */
+
 $router->get('/', function () {
-
-});
-
-$router->get('/stations', function () {
-    $controller = new \App\Controllers\ApiController();
-    $controller->stations();
+    $controller = new StationsController();
+    $controller->index();
 });
 
 $router->get('/stations/:id', function ($id) {
-    $controller =  new \App\Controllers\ApiController();
-    $controller->station($id);
+    $controller = new StationsController();
+    $controller->show($id);
 });
 
 $router->get('/releves/:station', function ($station) {
+    // formulaire d'ajout
+});
 
-    var_dump($station);
+/**
+ * Routage API
+ */
 
-    $controller =  new \App\Controllers\ApiController();
+$router->get('/api', function () {
+    $controller = new ApiController();
+    $controller->doc();
+});
+
+$router->get('/api/stations', function () {
+    $controller = new ApiController();
+    $controller->stations();
+});
+
+$router->get('/api/stations/:id', function ($id) {
+    $controller =  new ApiController();
+    $controller->station($id);
+});
+
+$router->get('/api/releves/:station', function ($station) {
+    $controller =  new ApiController();
     $controller->releves($station);
 });
+
+$router->post('/api/releves/:station', function ($station) {
+    $controller =  new ApiController();
+    $controller->add($station);
+});
+
 
 $router->run();
