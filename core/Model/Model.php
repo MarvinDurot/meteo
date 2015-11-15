f15cd75e6fb4904e2867413d6f2887bf10e776f6
MarvinDuroy <marvin.dut@gmail.com>
2015-11-15 21:06:14 +0100

    Optimize Table class and Model class

--- core/Model/Model.php
+++ core/Model/Model.php
@@ -10,18 +10,26 @@ namespace Core\Model;
 class Model implements \JsonSerializable
 {
     /**
-     * Champs du JSON
+     * Champs du modèle
      * @var array
      */
-    protected $jsonnable = [];
+    protected $fields = [];
 
     /**
-     * Retourne les champs du modèle
+     * Constructeur
+     * @param $fields
+     */
+    public function __construct($fields) {
+        $this->fields = $fields;
+    }
+
+    /**
+     * Retourne tous les champs du modèle
      * @return array
      */
     public function getFields()
     {
-        return get_object_vars($this);
+        return $this->fields;
     }
 
     /**
@@ -30,10 +38,54 @@ class Model implements \JsonSerializable
      */
     public function jsonSerialize()
     {
-        return array_filter($this->getFields(), function ($key) {
-            return in_array($key, $this->jsonnable);
-        }, ARRAY_FILTER_USE_KEY);
+        // Filtrage des éléments vides
+        return array_filter($this->fields);
+    }
+
+    /**
+     * Rédéfinition ToString
+     * @return string
+     */
+    public function __toString() {
+        return implode("\t", $this->fields);
+    }
+
+    /**
+     * Getter
+     * @param $field
+     * @return mixed
+     * @throws ModelException
+     */
+    public function __get($field)
+    {
+        if (array_key_exists($field, $this->fields))
+            return $this->fields[$field];
+
+        throw new ModelException("Invalid field name $field");
+    }
+
+    /**
+     * Setter
+     * @param $field
+     * @param $value
+     * @throws ModelException
+     */
+    public function __set($field, $value)
+    {
+        if (! array_key_exists($field, $this->fields))
+            throw new ModelException("Invalid field name $field");
+
+        $this->fields[$field] = $value;
+    }
+
+    /**
+     * Rédéfinition isset
+     * @param $field
+     * @return bool
+     */
+    public function __isset($field)
+    {
+        return array_key_exists($field, $this->fields);
     }
 }
-
 ?>
--- core/Table/Table.php
+++ core/Table/Table.php
@@ -35,10 +35,9 @@ class Table
     protected $columns = [];
 
     /**
-     * Clauses
+     * Clause WHERE pour la clé primaire
      */
     protected $keyClause;
-    protected $updateClause;
 
     /**
      * Requêtes préparées
@@ -64,11 +63,10 @@ class Table
         $this->pdo = $pdo;
         $this->columns = $this->getColumns();
         $this->keyClause = $this->buildKeyClause();
-        $this->updateClause = $this->buildUpdateClause();
     }
 
     /**
-     * Retrouve les noms des champs de la table sans les clés
+     * Retrouve le nom des champs de la table sans les clés
      * @return array
      * @throws TableException
      */
@@ -81,19 +79,6 @@ class Table
 
         $columns = $stmt->fetchAll(\PDO::FETCH_COLUMN);
         return array_diff($columns, $this->key);
-    }
-
-    /**
-     * Construit la clause UPDATE
-     * @return string
-     */
-    private function buildUpdateClause()
-    {
-        return implode(',', array_map(
-                function ($column) {
-                    return "$column = :$column";
-                }, $this->columns)
-        );
     }
 
     /**
@@ -110,9 +95,39 @@ class Table
     }
 
     /**
-     * Récupère un enregistrement de la table
+     * Retourne la clé sous forme de tableau associatif
+     * @param $key
+     * @return array
+     */
+    private function getKey($key)
+    {
+        $values = (is_array($key)) ? $key : [$key];
+        return array_combine($this->key, $values);
+    }
+
+    /**
+     * Formate une liste de colonnes en chaîne
+     * (évite les injections SQL)
+     * @param $columns
+     * @return string
+     */
+    private function formatColumns($columns)
+    {
+        if (count($columns) > 0) {
+            $validColumns = array_intersect(
+                $this->key + $this->columns, $columns
+            );
+            return implode(',', $validColumns);
+        } else {
+            return '*';
+        }
+    }
+
+    /**
+     * Récupère un enregistrement à partir de sa clé
      * @param array|string $key
      * @return mixed
+     * @throws TableException
      */
     public function find($key)
     {
@@ -122,18 +137,19 @@ class Table
             $this->stmtFind = $this->pdo->prepare($sql);
         }
 
-        // Formatage de la clé primaire
-        $key = (is_array($key)) ? $key : [$key];
-        $key = array_combine($this->key, $key);
+        // Éxécution de la requête
+        $this->stmtFind->execute($this->getKey($key));
 
-        // Éxécution de la requête et récupération des résultats
-        $this->stmtFind->execute($key);
-        $this->stmtFind->setFetchMode(\PDO::FETCH_CLASS, $this->class);
-        return $this->stmtFind->fetch();
+        // Récupération des résultats
+        if (! $record = $this->stmtFind->fetch(\PDO::FETCH_ASSOC)) {
+            throw new TableException('No record found!');
+        }
+
+        return new $this->class($record);
     }
 
     /**
-     * Récupère plusieurs enregistrement selon un critère d'égalité
+     * Récupère plusieurs enregistrements selon un critère d'égalité
      * @param string $column
      * @param string $value
      * @param int $limit
@@ -143,47 +159,54 @@ class Table
     public function where($column, $value, $limit = 1000)
     {
         // Préparation de la requête
-        if (! isset($this->stmtWhere[$column])) {
-            // Pour éviter les injections on vérifie l'existence de la colonne
-            if (! in_array($column, array_merge($this->columns, $this->key)))
-                throw new TableException('Invalid column name!');
+        if (!isset($this->stmtWhere[$column]))
+            $this->newWhereStatement($column);
 
-            $sql = "SELECT * FROM $this->table WHERE $column = :value LIMIT :limit";
-            $this->stmtWhere[$column] = $this->pdo->prepare($sql);
-        }
-
-        // Éxécution de la requête et récupération des résultats
+        // Éxécution de la requête
         $this->stmtWhere[$column]->bindParam(':value', $value, \PDO::PARAM_STR, 12);
         $this->stmtWhere[$column]->bindParam(':limit', $limit, \PDO::PARAM_INT);
         $this->stmtWhere[$column]->execute();
-        return $this->stmtWhere[$column]->fetchAll(\PDO::FETCH_CLASS, $this->class);
+
+        // Récupération des résultats
+        if (! $res = $this->stmtWhere[$column]->fetchAll(\PDO::FETCH_ASSOC) ) {
+            throw new TableException('No record found!');
+        }
+
+        // Instanciation des objets
+        return array_map(function ($fields) {
+            return new $this->class($fields);
+        }, $res);
     }
 
     /**
      * Récupère tous les enregistrements de la table
-     * (Possibilité de définir les champs à récupérer)
+     * (possibilité de définir les champs à récupérer)
      * @return mixed
+     * @throws TableException
      */
     public function all()
     {
-        // Si colonnes explicites
-        if (func_num_args() > 0) {
-            // Récupération des colonnes à afficher
-            $columns = implode(',', array_intersect(
-                array_merge($this->key, $this->columns), func_get_args())
-            );
-        } else {
-            // Sinon on récupère tout
-            $columns = '*';
+        // Récupération des colonnes à afficher
+        $columns = $this->formatColumns(func_get_args());
+
+        // Éxécution de la requête
+        $stmt = $this->pdo->query("SELECT $columns FROM $this->table");
+
+        // Récupération des résultats
+        if (! $res = $stmt->fetchAll(\PDO::FETCH_ASSOC)) {
+            throw new TableException('No record found!');
         }
 
-        $stmt = $this->pdo->query("SELECT $columns FROM $this->table");
-        return $stmt->fetchAll(\PDO::FETCH_CLASS, $this->class);
+        // Instanciation des objets
+        return array_map(function ($fields) {
+            return new $this->class($fields);
+        }, $res);
     }
 
     /**
      * Met à jour un enregistrement en base
      * @param $obj mixed
+     * @return bool
      * @throws TableException
      */
     public function update($obj)
@@ -192,57 +215,43 @@ class Table
         if (!$obj instanceof $this->class)
             throw new TableException('Invalid object class!');
 
-        // Récupération des champs de l'objet
-        $fields = $obj->getFields();
-
         // Préparation de la requête
-        if ($this->stmtUpdate === null) {
-            $sql = "UPDATE $this->table SET $this->updateClause WHERE $this->keyClause";
-            $this->stmtUpdate = $this->pdo->prepare($sql);
-        }
+        if ($this->stmtUpdate === null)
+            $this->newUpdateStatement();
 
-        // Éxécution de la requête
-        if ($this->stmtUpdate->execute($fields) < 0)
-            throw new TableException();
+        try {
+            // Éxécution de la requête
+            return $this->stmtUpdate->execute($obj->getFields());
+        } catch (\PDOException $e) {
+            throw new TableException("Failed update!\n" . $e->getTraceAsString());
+        }
     }
 
     /**
-     * Insère un enregistrement en base et retourne le modèle associé
+     * Insère un enregistrement en base
+     * et retourne le modèle associé
      * @param array $fields
-     * @return Object
+     * @return bool
      * @throws TableException
      */
     public function create($fields)
     {
-        // Suppression des clés si auto incrément
-        if (! $this->increment) {
-            foreach($this->key as $key) {
-                unset($fields[$key]);
-            }
-        }
-
         // Préparation de la requête
-        if ($this->stmtCreate === null) {
-            $params = implode(',', array_fill(0, count($fields), '?'));
-            $sql = "INSERT INTO $this->table VALUES($params)";
-            $this->stmtCreate = $this->pdo->prepare($sql);
+        if ($this->stmtCreate === null)
+            $this->newInsertStatement();
+
+        try {
+            // Exécution de la requête
+            return $this->stmtCreate->execute($fields);
+        } catch (\PDOException $e) {
+            throw new TableException("Failed insert!\n" . $e->getTraceAsString());
         }
-
-        // Exécution de la requête
-        if ($this->stmtCreate->execute($fields) < 0)
-            throw new TableException('Insert fails!');
-
-        // Récupération de la clé auto incrémentée
-        if ($this->increment) {
-            $fields[$this->key[0]] = $this->pdo->lastInsertId();
-        }
-
-        return new $this->class($fields);
     }
 
     /**
      * Supprime un enregistrement en base
      * @param array|string $key
+     * @return bool
      * @throws TableException
      */
     public function delete($key)
@@ -253,12 +262,69 @@ class Table
             $this->stmtDelete = $this->pdo->prepare($sql);
         }
 
-        // Formatage de la clé primaire
-        $key = (is_array($key)) ? $key : [$key];
-        $key = array_combine($this->key, $key);
+        try {
+            // Exécution de la requête
+            return $this->stmtDelete->execute($this->getKey($key));
+        } catch (\PDOException $e) {
+            throw new TableException("Failed deletion!\n" . $e->getTraceAsString());
+        }
+    }
 
-        // Exécution de la requête
-        if ($this->stmtDelete->execute($key) < 0)
-            throw new TableException('Deletion fails!');
+    /**
+     * Préparation d'une requête WHERE
+     * @param $column
+     * @throws TableException
+     */
+    private function newWhereStatement($column)
+    {
+        // Pour éviter les injections on vérifie l'existence de la colonne
+        if (!in_array($column, $this->columns + $this->key))
+            throw new TableException('Invalid column name!');
+
+        $sql = "SELECT * FROM $this->table WHERE $column = :value LIMIT :limit";
+        $this->stmtWhere[$column] = $this->pdo->prepare($sql);
+    }
+
+    /**
+     * Préparation de la clause UPDATE
+     * @return string
+     */
+    private function newUpdateStatement()
+    {
+        // Préparation de la clause
+        $clause = implode(', ', array_map(
+                function ($column) {
+                    return "$column = :$column";
+                }, $this->columns)
+        );
+
+        // Création de la requête préparée
+        $sql = "UPDATE $this->table SET $clause WHERE $this->keyClause";
+        $this->stmtUpdate = $this->pdo->prepare($sql);
+    }
+
+    /**
+     * Préparation de la clause INSERT
+     * @return string
+     */
+    private function newInsertStatement()
+    {
+        // Récupération du nom des colonnes
+        $columns = (! $this->increment) ?
+            $this->key + $this->columns :
+            $this->columns;
+
+        $columnNames = implode(', ', $columns);
+
+        // Création des paramètres
+        $params = implode(', ', array_map(
+                function ($column) {
+                    return ":$column";
+                }, $columns)
+        );
+
+        // Création de la requête préparée
+        $sql = "INSERT INTO $this->table ($columnNames) VALUES($params)";
+        $this->stmtCreate = $this->pdo->prepare($sql);
     }
 }
