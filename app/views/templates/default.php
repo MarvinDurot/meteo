272bfd4abf5145edf65ca00f8a73338cd51a28f1
MarvinDurot <marvin.dut@gmail.com>
2015-11-17 01:55:13 +0100

    Add git link

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -19,6 +19,7 @@
 <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="<?= HOME; ?>" class="brand-logo">Météo</a>
         <ul class="right hide-on-med-and-down">
+            <li><a href="https://github.com/MarvinDurot/meteo">GitHub</a></li>
             <li><a href="<?= HOME; ?>/api">API</a></li>
         </ul>
 
