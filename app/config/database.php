195a08104d00aa19bbee2c80c59dc9f0a0b67e1f
Marvin <MarvinDurot@users.noreply.github.com>
2015-11-17 01:42:55 +0100

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => 'root',
-    'database' => 'meteo'
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'database' => ''
 );
